3d4bd6fc3bf3228434904cb4bbe72886e1843917
fan <fanhouin@gmail.com>
2022-07-19 18:20:20 +0800

    add gitlab

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 
 ## Services
 ```bash
-spree:4000      @ 4.4.0     - 1
-magento2:80/443 @ 2.4.4     - 2
-petstore:8001   @ 1.0.11    - 3
-onos:8002   	@ 2.5.8     - 4
-redmine:8003    @ 5.0.2     - 5
+petstore:8001   @ 1.0.11	- 1
+spree:4000      @ 4.4.0 	- 2
+magento2:80/443 @ 2.4.4		- 3
+gitlab:80/443 	@ 15.1.2	- 4
+onos:8002   	@ 2.5.8		- 5
+redmine:8003    @ 5.0.2		- 6
 ```
 ## Setup
 ```bash
--- service_down.sh
+++ service_down.sh
@@ -3,34 +3,40 @@ if [ $# -eq 0 ]
   then
     echo "Usage: service_up.sh <service_num>
 Service_num: 
-	spree:4000      @ 4.4.0 	- 1
-	magento2:80/443 @ 2.4.4		- 2
-	petstore:8001   @ 1.0.11	- 3
-	onos:8002   	@ 2.5.8		- 4
-	redmine:8003    @ 5.0.2		- 5"
+	petstore:8001   @ 1.0.11	- 1
+	spree:4000      @ 4.4.0 	- 2
+	magento2:80/443 @ 2.4.4		- 3
+	gitlab:80/443 	@ 15.1.2	- 4
+	onos:8002   	@ 2.5.8		- 5
+	redmine:8003    @ 5.0.2		- 6"
 	exit 
 fi
 
 case $1 in
 1)
-	cd spree/spree_starter-main &&\
-	docker-compose down
-	;;
-2)
-	cd magento2 && \
-	docker-compose down
-	;;
-3)
 	cd petstore && \
 	docker-compose down
 	;;	
+2)
+	cd spree/spree_starter-main &&\
+	docker-compose down
+	;;
+3)
+	cd magento2 && \
+	docker-compose down
+	;;
 4) 
-	cd onos	&& \
+	cd gitlab	&& \
 	docker-compose down
 	;;
 5) 
+	cd onos	&& \
+	docker-compose down
+	;;
+6) 
 	cd redmine	&& \
 	docker-compose down
 	;;
+
 esac
 
